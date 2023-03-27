7a7cebbf8c37d7cddb3d0bd4fc02d55f03c4a025
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-03-27 21:39:22 +0300

    update

--- src/js/weather-widget-api.js
+++ src/js/weather-widget-api.js
@@ -1,41 +1,57 @@
 export default class WeatherApiService {};
-import WeatherApiService from "./geolocation"
 
 const apiKey = 'e3cf51b9d19c1b71b5828e7d427ff71f';
-const city = 'Kyiv';
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.7641856&lon=25.362432&appid=${apiKey}&units=metric&`;
-// const lat = crd.latitude;
-// const lon = ;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const weather = document.querySelector('.counteiner');
-    const { name, weather: [ {main, icon } ], main: { temp } } = data;
-    const roundedTemp = Math.round(temp);
-    const date = new Date();
-          weather.innerHTML = `
-          <div class="weather-info">
-          <h2 class="weather-temperature">${roundedTemp}&#176</h2>
-          <div class="weather-conloc">
-            <h3 class="weather-condition">${main}</h3>
-          <p class="location">
-            <svg class="icon" width="18" height="18">
-                <use href="./images/sprite.svg#icon-location"></use>
-            </svg>
-            ${name}
-          </p>
+function success(pos) {
+  const crd = pos.coords;
+  let lat = crd.latitude;
+  let lon = crd.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = document.querySelector('.weather-counteiner');
+      const { name, weather: [ {main, icon } ], main: { temp } } = data;
+      const roundedTemp = Math.round(temp);
+
+      const date = new Date();
+      const dateString = date.toDateString();
+      const [dayOfWeek, month, dayOfMonth, year] = dateString.split(' ');
+
+      weather.innerHTML = `
+            <li class="list">
+            <div class="weather-info">
+            <h2 class="weather-temperature">${roundedTemp}&#176</h2>
+            <div class="weather-conloc">
+              <h3 class="weather-condition">${main}</h3>
+            <p class="weather-location">
+              <svg class="icon" width="18" height="18">
+                  <use href="./images/sprite.svg#icon-location"></use>
+              </svg>
+              ${name}
+            </p>
+            </div>
           </div>
+          <div class="weather-icon">
+            <img src="https://openweathermap.org/img/wn/${icon}@4x.png" alt="${main}">
+          </div>
+          <p class="weather-data-time">${dayOfWeek} <br> ${dayOfMonth} ${month} ${year}</p>
         </div>
-        <div class="weather-icon">
-          <img src="https://openweathermap.org/img/wn/${icon}@4x.png" alt="">
-        </div>
-        <ul class="list data-time">
-          <li class="">${date.toDateString()}</li>
-        </ul>
-      </div>
-          `;
-  })
-  .catch(error => console.log(error));
+        </li>
+            `;
+    })
+    .catch(error => console.log(error));
+}
 
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
 
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 0,
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
